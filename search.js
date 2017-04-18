10752d3eab2d8b9212fda5ba713394a55e9b6938
Naveed Hasan <712.naveed@outlook.com>
2017-04-18 10:25:03 +0530

    Fix for Arrays

--- search.js
+++ search.js
@@ -1,16 +1,16 @@
 //needle - string to search for
 //hs - haystack
 search = function(needle,hs){
-
 	if(Array.isArray(hs)){
-		hs.forEach(function(x){
 		var res=[];
+		hs.forEach(function(x){
 		var s=search(needle,x);
+
 		if(s){
-			res.push(search(needle,x))
+			res.push(s)
 		}
 	});
-	if(res==[]){
+	if(res.length === 0){
 		return false
 	}else{
 		return res
@@ -19,10 +19,13 @@ search = function(needle,hs){
 	else if(typeof hs=="object"&&typeof hs!==null){
 		var res={};
 		for(var key in hs){
+
 			if(search(needle,key)){
+
 				res[key]=hs[key]
 			}else{
 				s=search(needle,hs[key]);
+
 				if(s){
 					res[key]=hs[key]
 				}
@@ -31,6 +34,7 @@ search = function(needle,hs){
 		if(JSON.stringify(res)=='{}'){
 			return false
 		}else{
+
 			return res
 		}
 	}
@@ -39,4 +43,33 @@ search = function(needle,hs){
 	}
 	return false
 	}
-
+/* Example: For Testing
+x={
+  "status": "success",
+  "data": {
+    "title": "sometitle",
+    "value_1": 0,
+    "value_2": 0,
+    "host": "somehost",
+    "obj": {"value_1":0},
+	  "url": "/something"
+	},
+  "data2": {
+    "title": "sometitle",
+    "value_1": 0,
+    "value_2": 0,
+    "host": "somehost",
+    "obj": {"value_1":["srch","t1"]},
+	  "url": "/something"
+	},
+  "data3": {
+    "title": "sometitle55",
+    "value_155": 0,
+    "value_255": 0,
+    "host": "somehost55",
+    "obj": {"value_155":0},
+	  "url": "/something55"
+	}
+	}
+y=search("srch",x)
+*/
